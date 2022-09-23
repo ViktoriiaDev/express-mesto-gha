ec21097b8e13b6606acc8e0cf57c18e0b00116ec
Viktoria Kotenko <kvi6605@gmail.com>
2022-09-24 00:22:52 +0300

    Changed user scheme, added login controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,7 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+
 const {
   incorrectDataError,
   dataNotFoundError,
@@ -10,11 +13,9 @@ module.exports.getUsers = (req, res) => {
     .then((users) => res.send({ data: users }))
     .catch((error) => {
       if (error.name === 'ValidationError') {
-        res
-          .status(incorrectDataError)
-          .send({
-            message: 'Переданы некорректные данные при создании пользователя',
-          });
+        res.status(incorrectDataError).send({
+          message: 'Переданы некорректные данные при создании пользователя',
+        });
       } else {
         res
           .status(serverError)
@@ -31,15 +32,11 @@ module.exports.getUserById = (req, res) => {
     .then((user) => res.send({ data: user }))
     .catch((error) => {
       if (error.name === 'CastError') {
-        res
-          .status(incorrectDataError)
-          .send({
-            message: 'Переданы некорректные данные для получения пользователя',
-          });
+        res.status(incorrectDataError).send({
+          message: 'Переданы некорректные данные для получения пользователя',
+        });
       } else if (error.name === 'Error') {
-        res
-          .status(dataNotFoundError)
-          .send({ message: error.message });
+        res.status(dataNotFoundError).send({ message: error.message });
       } else {
         res
           .status(serverError)
@@ -49,16 +46,20 @@ module.exports.getUserById = (req, res) => {
 };
 
 module.exports.createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
-  User.create({ name, about, avatar })
+  const {
+    name, about, avatar, email, password,
+  } = req.body;
+  bcrypt
+    .hash(password, 10)
+    .then((hash) => User.create({
+      name, about, avatar, email, password: hash,
+    }))
     .then((user) => res.send({ data: user }))
     .catch((error) => {
       if (error.name === 'ValidationError') {
-        res
-          .status(incorrectDataError)
-          .send({
-            message: 'Переданы некорректные данные при создании пользователя',
-          });
+        res.status(incorrectDataError).send({
+          message: 'Переданы некорректные данные при создании пользователя',
+        });
       } else {
         res
           .status(serverError)
@@ -85,11 +86,9 @@ module.exports.patchUser = (req, res) => {
           .status(dataNotFoundError)
           .send({ message: 'Пользователь с данным _id не найден' });
       } else if (error.name === 'ValidationError') {
-        res
-          .status(incorrectDataError)
-          .send({
-            message: 'Переданы некорректные данные при обновлении пользователя',
-          });
+        res.status(incorrectDataError).send({
+          message: 'Переданы некорректные данные при обновлении пользователя',
+        });
       } else {
         res
           .status(serverError)
@@ -112,15 +111,31 @@ module.exports.patchUserAvatar = (req, res) => {
           .status(dataNotFoundError)
           .send({ message: 'Пользователь с данным _id не найден' });
       } else if (error.name === 'ValidationError') {
-        res
-          .status(incorrectDataError)
-          .send({
-            message: 'Переданы некорректные данные при обновлении аватара',
-          });
+        res.status(incorrectDataError).send({
+          message: 'Переданы некорректные данные при обновлении аватара',
+        });
       } else {
         res
           .status(serverError)
           .send({ message: 'На сервере произошла ошибка' });
       }
+    });
+};
+
+module.exports.login = (req, res) => {
+  const { email, password } = req.body;
+
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      // создадим токен
+      const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
+      res.cookie('token', '12345ABCDE', { maxAge: 3600 * 24, httpOnly: true });
+      // вернём токен
+      res.send({ token });
+    })
+    .catch((err) => {
+      res
+        .status(401)
+        .send({ message: err.message });
     });
 };
